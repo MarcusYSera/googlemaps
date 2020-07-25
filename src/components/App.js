b0c62cd8aa388bff1f29c6df18cb24496685da82
Marcus Sera <m.yoshiosera@gmail.com>
2020-07-24 22:01:27 -0700

    working on maps rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount(){
+    initMap();
+  }
+  initMap=()=>{
+    let map = new google.maps.Map(document.getElementById("map"))
+  }
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        <div id="map"></div>
+      </div>
+    );
+  }
+}
 
 export default App;
