add297b1c7c12dac77b60a4d6cf0579a54a2c6b2
Marcus Sera <m.yoshiosera@gmail.com>
2020-07-27 22:57:07 -0700

    truncating model criteria

--- server/models/pinlocation.js
+++ server/models/pinlocation.js
@@ -1,3 +1,5 @@
+// https://developers.google.com/places/web-service/details#PlaceDetailsResponses
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; 
 
@@ -6,20 +8,23 @@ const PinLocationSchema = new Schema({
     type: String, 
     required: true
   },
-  name: {
-    type: String, 
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  website: {
-    type: String, 
-    required: true
-  },
-  phone: {
-    type: String,
-    required: true
-  }
+  // name: {
+  //   type: String, 
+  //   required: true
+  // },
+  // description: {
+  //   type: String,
+  //   required: true
+  // },
+  // website: {
+  //   type: String, 
+  //   required: true
+  // },
+  // phone: {
+  //   type: String,
+  //   required: true
+  // }
 })
+
+const PinLocation = mongoose.model('pinlocation', PinLocationSchema);
+module.exports = PinLocation;
